3cb10434a48067e1f77f46420304b1cf22c52ac8
Shipply <44271484+Shippli@users.noreply.github.com>
2026-09-23 23:20:15 +0200

    web: define shadcn lint rules

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,4 +1,5 @@
 import antfu from '@antfu/eslint-config';
+import { plugin as shadcn } from '@shadcn/lint';
 
 export default antfu({
   stylistic: {
@@ -30,5 +31,38 @@ export default antfu({
   rules: {
     // Sometimes for formatting amounts of dollar, we need "${{value}}" as value is interpolated later, it's not a template string here
     'no-template-curly-in-string': 'off',
+  },
+}, {
+  files: ['**/*.{ts,tsx}'],
+  plugins: {
+    shadcn,
+  },
+  rules: {
+    'shadcn/no-restyle': ['error', { allow: ['layout'] }],
+    'shadcn/no-raw-colors': 'error',
+    'shadcn/no-arbitrary-values': ['error', {
+      allow: ['max-h-[*vh]', 'grid-cols-[1fr_auto]'],
+    }],
+    'shadcn/no-inline-styles': 'error',
+    'shadcn/no-unknown-classes': ['error', {
+      allow: [
+        'i-tabler-*',
+        'animate-in',
+        'animate-out',
+        'fade-in-*',
+        'fade-out-*',
+        'zoom-in-*',
+        'zoom-out-*',
+        'slide-in-from-*',
+        'slide-out-to-*',
+        'pixelated',
+      ],
+    }],
+    'shadcn/require-static-classes': 'error',
+  },
+}, {
+  files: ['src/client/components/ui/**'],
+  rules: {
+    'shadcn/no-restyle': 'off',
   },
 });
